1225ccc99fb54af5f5de2b07ec6f0ce1d88eee45
Omar <omaar_5@hotmail.com>
2021-05-14 16:35:06 +0200

    feat: Header part-2

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,8 +1,25 @@
+import {
+    HomeIcon,
+    LightningBoltIcon,
+    BadgeCheckIcon,
+    CollectionIcon,
+    SearchIcon,
+    UserIcon,
+} from "@heroicons/react/outline";
 import Image from "next/image";
+import { HeaderItem } from "..";
 
 const Header = () => {
     return (
-        <header className="">
+        <header className="header">
+            <div className="header__icons">
+                <HeaderItem title="HOME" Icon={HomeIcon} />
+                <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+                <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+                <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+                <HeaderItem title="SEARCH" Icon={SearchIcon} />
+                <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+            </div>
             <Image
                 className="object-contain"
                 src="/hulu.png"
