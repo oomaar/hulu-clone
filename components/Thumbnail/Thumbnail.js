69f8d6a9bc26fba1cd760df5ea39d80923e09859
Omar <omaar_5@hotmail.com>
2021-05-14 21:30:23 +0200

    feat: Thumbnail component

--- components/Thumbnail/Thumbnail.js
+++ components/Thumbnail/Thumbnail.js
@@ -1,7 +1,35 @@
-const Thumbnail = () => {
+import Image from "next/image";
+import { ThumbUpIcon } from "@heroicons/react/outline";
+
+const Thumbnail = ({ result }) => {
+    const BASE_URL = "https://image.tmdb.org/t/p/original"
     return (
-        <div>
-            Thumbnail
+        <div className="p-2 group cursor-pointer transition duration-200 ease-in transform 
+        sm:hover:scale-105 hover:z-50">
+            <Image
+                src={
+                    `${BASE_URL}${result.backdrop_path || result.poster_path}`
+                    || `${BASE_URL}${result.poster_path}`
+                }
+                layout="responsive"
+                width={1920}
+                height={1080}
+            />
+            <div>
+                <p className="truncate max-w-md">{result.overview}</p>
+                <h2 className="mt-1 text-2xl text-white
+                transition-all duration-100 ease-in-out group-hover:font-bold">
+                    {result.title || result.original_name}
+                </h2>
+                <p className="flex items-center opacity-0 group-hover:opacity-100">
+                    {result.media_type && `${result.media_type} .`}{" "}
+                    {result.release_date || result.first_air_date} . {" "}
+                    <ThumbUpIcon
+                        style={{ height: "1.25rem", margin: "0 0.5rem" }}
+                        className="h-5 mx-2"
+                    /> {result.vote_count}
+                </p>
+            </div>
         </div>
     );
 };
