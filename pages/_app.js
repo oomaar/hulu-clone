b8450fbee19ed04b3a44db4ff057b382d2d8ff89
Omar <omaar_5@hotmail.com>
2021-05-14 15:03:27 +0200

    feat: initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+function HuluApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default HuluApp;
