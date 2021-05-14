2c0d558f9895a20fbca28fdcf105306434921302
Omar <omaar_5@hotmail.com>
2021-05-14 16:34:47 +0200

    feat: headerItem component

--- components/HeaderItem/HeaderItem.js
+++ components/HeaderItem/HeaderItem.js
@@ -1,7 +1,9 @@
-const HeaderItem = () => {
+const HeaderItem = ({ title, Icon }) => {
     return (
-        <div>
-            HeaderItem
+        <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 
+        hover:text-white">
+            <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+            <p className="tracking-widest opacity-0 group-hover:opacity-100">{title}</p>
         </div>
     );
 };
