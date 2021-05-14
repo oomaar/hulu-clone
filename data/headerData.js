fd96af6bd1c9bc72e18334d0debd3557e1dc0408
Omar <omaar_5@hotmail.com>
2021-05-14 17:01:42 +0200

    feat: headerData file

---
+++ data/headerData.js
@@ -0,0 +1,41 @@
+import {
+    HomeIcon,
+    LightningBoltIcon,
+    BadgeCheckIcon,
+    CollectionIcon,
+    SearchIcon,
+    UserIcon,
+} from "@heroicons/react/outline";
+
+export const headerData = [
+    {
+        id: 1,
+        title: "HOME",
+        icon: HomeIcon
+    },
+    {
+        id: 2,
+        title: "TRENDING",
+        icon: LightningBoltIcon
+    },
+    {
+        id: 3,
+        title: "VERIFIED",
+        icon: BadgeCheckIcon
+    },
+    {
+        id: 4,
+        title: "COLLECTIONS",
+        icon: CollectionIcon
+    },
+    {
+        id: 5,
+        title: "SEARCH",
+        icon: SearchIcon
+    },
+    {
+        id: 6,
+        title: "ACCOUNT",
+        icon: UserIcon
+    }
+];
