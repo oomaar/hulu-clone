e8e372a79bac799dda7871fc617a4bd9ea18ad52
Omar <omaar_5@hotmail.com>
2021-05-14 20:09:01 +0200

    feat: serverSideRender tmdb data

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
 
 import Image from 'next/image'
 import { GlobalHeadTag, Header, Navbar, Results } from '../components'
+import requests from '../helpers/requests';
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <GlobalHeadTag title="hulu 2.0" />
       <Header />
       <Navbar />
-      <Results />
+      <Results results={results} />
     </div>
   )
-}
+};
+
+export async function getServerSideProps (context) {
+  const genre = context.query.genre;
+
+  const request = await 
+  fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`)
+  .then(res => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    }
+  };
+};
