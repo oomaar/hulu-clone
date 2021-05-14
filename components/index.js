cfe954210ebaa0d948690337c9c4975d45f8782a
Omar <omaar_5@hotmail.com>
2021-05-14 17:04:06 +0200

    feat: Navbar part-1

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export { default as GlobalHeadTag } from './GlobalHeadTag/GlobalHeadTag';
 export { default as Header } from './Header/Header';
 export { default as HeaderItem } from './HeaderItem/HeaderItem';
+export { default as Navbar } from './Navbar/Navbar';
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 
 import Image from 'next/image'
-import { GlobalHeadTag, Header } from '../components'
+import { GlobalHeadTag, Header, Navbar } from '../components'
 
 export default function Home() {
   return (
     <div>
       <GlobalHeadTag title="hulu 2.0" />
       <Header />
+      <Navbar />
     </div>
   )
 }
