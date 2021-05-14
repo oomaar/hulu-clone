0fa592cfe4588325addd693af4f65c4734628447
Omar <omaar_5@hotmail.com>
2021-05-14 16:38:20 +0200

    refactor: headerItem component

--- components/HeaderItem/HeaderItem.js
+++ components/HeaderItem/HeaderItem.js
@@ -1,7 +1,6 @@
 const HeaderItem = ({ title, Icon }) => {
     return (
-        <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 
-        hover:text-white">
+        <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white">
             <Icon className="h-8 mb-1 group-hover:animate-bounce" />
             <p className="tracking-widest opacity-0 group-hover:opacity-100">{title}</p>
         </div>
